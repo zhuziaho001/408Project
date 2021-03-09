6a38ab0583846ffe5c232c38f3652a815b527826
CN-LeiKa <37507468+zhuziaho001@users.noreply.github.com>
2021-03-09 09:20:29 +0800

    单链表

--- 数据结构/线性表/单链表.c
+++ 数据结构/线性表/单链表.c
@@ -1,81 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ElemType int
-#define List LNode
-
 typedef struct LNode
 {
-    ElemType data;
+    int data;
     struct LNode *next;
-} *LinkList;
+} LinkList;
 
-/************************
-*名称:TraverseList
-*功能:遍历链表
-*参数1:
-*参数2:
-************************/
-void TraverseList(LinkList L)
-{
-    LinkList ptr = L->next;
-    if (ptr == NULL) {
-        printf("链表为空\n");
-    }
-
-    while (ptr != NULL) {
-        printf("%d ", ptr->data);
-        ptr = ptr->next;
-    }
-}
 /************************
 *名称:
-*功能:
+*功能:头插法
 *参数1:
 *参数2:
 ************************/
-LinkList List_HeadInsert(LinkList L)
+LinkList *HeadInsert(LinkList *L)
 {
-    LinkList s;
-    int x;
-    L = (LinkList)malloc(sizeof(LinkList));
+    LinkList *temp;
+    int t;
     L->next = NULL;
-    scanf("%d", &x);
-    while (x != -1)
+    L->data = t;
+    scanf("%d", &t);
+    while (t != -1)
     {
-        s = (LinkList)malloc(sizeof(LinkList));
-        s->data = x;
-        s->next = L->next;
-        L->next = s;
-        scanf("%d", &x);
+        temp = (LinkList *)malloc(sizeof(LinkList));
+        temp->data = t;
+        temp->next = L->next;
+        L->next = temp;
+        scanf("%d", &t);
     }
     return L;
-}
-/************************
-*名称:GetElem
-*功能:
-*参数1:
-*参数2:
-************************/
-LinkList GetElem(LinkList L,int i){
-    int j=1;
-    LinkList p=L->next;
-    if(i==0)return L;
-    if(i<1)return NULL;
-    while (p&&j<1)
-    {
-        p=p->next;
-        j++;
-    }
-    return p;
 }
 
 int main()
 {
-    LinkList L;
-    L = List_HeadInsert(L);
-    TraverseList(L);
-    L=GetElem(L,3);
-    printf("%d",L->data); 
-
+    LinkList *L = (LinkList *)malloc(sizeof(LinkList));
+    HeadInsert(L);
+    L = L->next;
+    printf("%d\n", L->data);
+    free(L);
+    printf("%d\n", L->data);
     return 0;
 }
